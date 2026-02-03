eb8ecef2ec9a0931ef0d83ce85e29e1612063417
georgiamal <144332277+georgiamal@users.noreply.github.com>
2026-02-03 23:30:02 +0000

    gitignore

---
+++ .gitignore.py
@@ -0,0 +1,23 @@
+# Byte-compiled / optimized / DLL files
+__pycache__/
+*.py[cod]
+*$py.class
+
+# PyCharm project files
+.idea/
+*.iml
+
+# Virtual environment
+venv/
+.env/
+.env.*
+
+# Logs
+*.log
+
+# Exported data
+data/kindle_books.csv
+
+# OS-specific files
+.DS_Store
+Thumbs.db
